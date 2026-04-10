aeca779decb4c2172456ff0c5653a03536d6c921
Masic Din <D.Masic@htlvb.at>
2026-04-10 08:49:01 +0200

    Add PortScanner and improvements

---
+++ IP-Adressen Scanner/DindoServices/Portscanner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace IP_Adressen_Scanner.DindoServices
+{
+    internal class Portscanner
+    {
+        public class PortScanner
+        {
+            public bool IsPortOpen(string ip, int port)
+            {
+                try
+                {
+                    using (TcpClient client = new TcpClient())
+                    {
+                        var result = client.BeginConnect(ip, port, null, null);
+                        return result.AsyncWaitHandle.WaitOne(300);
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ IP-Adressen Scanner/DindoServices/ScanResults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IP_Adressen_Scanner.DindoServices
+{
+    internal class ScanResults
+    {
+        public class ScanResult
+        {
+            public string Ip { get; set; }
+            public int Port { get; set; }
+            public bool IsOpen { get; set; }
+        }
+    }
+}
